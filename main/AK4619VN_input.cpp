8a49d46135742b1b259ff3946d8fb9b1cd4265a0
jdaremes <samesjemerad@gmail.com>
2025-09-17 16:38:26 -0600

    Some buffer pseudocode (AK4619VN_input -> AK4619VN_buffers?)

--- main/AK4619VN_input.cpp
+++ main/AK4619VN_input.cpp
@@ -2,6 +2,8 @@
 
 #include "driver/gpio.h"
 #include "freertos/task.h"
+#include "freertos/stream_buffer.h"
+#include "freertos/semphr.h"
 #include "esp_check.h"
 
 #include "driver/spi_master.h"
@@ -12,7 +14,51 @@
 #include "esp_log.h"
 #include "math.h"
 
-#define STREAM_BUFFER_LENGTH_BYTES      ( (size_t) SAMPLE_COUNT )
+#define STREAM_BUFFER_LENGTH_BYTES      ( (size_t) SAMPLE_COUNT * 3 * 4) // Divided by 8?
+#define STREAM_BUFFER_TRIGGER_LEVEL     ( (BaseType_t) 10)
+
+static StreamBufferHandle_t xAK4619VN_input = NULL;
+
+static SemaphoreHandle_t buf1ready, 
+                         buf2ready          = NULL;
+
+static SemaphoreHandle_t bufReady = NULL;
+volatile int bufFlag;
+
+
+/*
+* Listener task
+*/
+static void interruptTriggerTask( void * pvParameters )
+{
+
+}
+
+
+/*
+* Function that handles 
+*/
+static void bufFunction( void * pvParameters ) 
+{
+    for( ;; )
+    {
+        // start read buf1
+        // dma_read(buf1)
+        
+        // Wait for DMA to interrupt (buf1 ready)
+        xSemaphoreTake(bufReady, portMAX_DELAY);
+        
+        // 0 = buf1, 1 = buf2
+        // can cast like true or false in C++
+        if (!bufFlag) {
+            // process_buf(buf1)
+        } else {
+            // process_buf(buf2)
+        }
+    }
+
+    vTaskDelete( NULL );
+}
 
 // Core 0 for all buffering
 /*
@@ -33,23 +79,25 @@ int AK4619VN_input::size() {
 * - This function may block if the buffer is full.
 */
 QuadSample AK4619VN_input::nextSample(const QuadSample& next) {
-    /*
-    task () {
-        while(1) {
-            start read buf1;
-            wait buf1 interrupt;
-            mark buf1 ready;
-            repeat for buf2;
+    xAK4619VN_input = xStreamBufferCreate(
+        STREAM_BUFFER_LENGTH_BYTES,
+        STREAM_BUFFER_TRIGGER_LEVEL
+    );
+
+    /* 
+    if (currentBuffer == NULL || currentIndex >= BUFFER_LEN)
+        // Signal from ISR
+        xSemaphoreTake(bufReady, portMAX_DELAY)
+
+        if (!bufFlag) {
+            currentBuffer = buf1;
+        } else if (readyFlag == 2) {
+            currentBuffer = buf2; 
         }
-    }
-    Next sample:
-    if reading buf1 {
-        wait buf1 ready;
-        return buf1[next sample]
-    } else {
-        repeat for buf2;
-    }
+
+        currentIndex = 0;
     */
+    
     return QuadSample();
 }
 
@@ -69,10 +117,30 @@ QuadIntSample AK4619VN_input::nextIntSample() {
 * It will be called once so it is the responsibility of the implementation to ensure that the buffer is continued to be filled.
 */
 void AK4619VN_input::start() {
-    // Free RTOS start protocol
-    xAK4619VN_input = xStreamBufferCreate(
-        
-    )
+    // Create semaphores to sync DMA completion
+    buf1ready = xSemaphoreCreateBinary();
+    buf2ready = xSemaphoreCreateBinary();
+
+    xTaskCreatePinnedToCore( 
+        bufFunction,
+        "ReadIntoBuf1",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + 2,
+        NULL,
+        0
+    );
+    
+    // (If needed) Interrupt Task
+    xTaskCreatePinnedToCore(
+        interruptTriggerTask,
+        "TrigInterr",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        configMAX_PRIORITIES - 1,
+        NULL,
+        0
+    );
 
 }
 
@@ -81,6 +149,7 @@ void AK4619VN_input::start() {
 */
 void AK4619VN_input::stop() {
     // Free RTOS stop protocol
+
 }
 
 /*
