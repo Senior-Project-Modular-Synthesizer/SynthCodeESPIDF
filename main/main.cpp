e4fc84e54a634828dc535955053a030d9c0aef55
Lukej142 <lukejones142@gmail.com>
2025-11-13 16:02:59 -0700

    Copy ui files

--- main/main.cpp
+++ main/main.cpp
@@ -2,5 +2,5 @@
 
 extern "C" void app_main(void)
 {
-    screen_main();
+    synth_gui();
 }
---
+++ main/ui/ui.h
@@ -0,0 +1,122 @@
+/**
+ * @file ui.h
+ */
+
+#ifndef UI_H
+#define UI_H
+
+#ifndef UI_SUBJECT_STRING_LENGTH
+#define UI_SUBJECT_STRING_LENGTH 256
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/*-------------------
+ * Permanent screens
+ *------------------*/
+extern lv_obj_t * home;
+
+/*----------------
+ * Global styles
+ *----------------*/
+
+
+/*----------------
+ * Fonts
+ *----------------*/
+extern lv_font_t * font_title;
+extern lv_font_t * font_subtitle;
+
+/*----------------
+ * Images
+ *----------------*/
+extern const void * img_wifi;
+extern const void * img_bluetooth;
+extern const void * img_bell;
+
+/*----------------
+ * Subjects
+ *----------------*/
+extern lv_subject_t hours;
+extern lv_subject_t mins;
+extern lv_subject_t age;
+extern lv_subject_t bluetooth_on;
+extern lv_subject_t wifi_on;
+extern lv_subject_t notification_on;
+extern lv_subject_t hour_edited;
+extern lv_subject_t min_edited;
+extern lv_subject_t arc1;
+extern lv_subject_t arc2;
+extern lv_subject_t slide;
+extern lv_subject_t check;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/*----------------
+ * Event Callbacks
+ *----------------*/
+void arc_changed(lv_event_t * e);
+
+/**
+ * Initialize the component library
+ */
+
+void Project_init_gen(const char * asset_path);
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   POST INCLUDES
+ **********************/
+
+/*Include all the widget and components of this library*/
+#include "components/arc/arc_gen.h"
+#include "components/checkbox/checkbox_gen.h"
+#include "components/column/column_gen.h"
+#include "components/effectslider/effectslider_gen.h"
+#include "components/grid/grid_gen.h"
+#include "components/header/header_gen.h"
+#include "components/icon/icon_gen.h"
+#include "components/row/row_gen.h"
+#include "components/subtitle/subtitle_gen.h"
+#include "components/title/title_gen.h"
+#include "screens/effect/effect_gen.h"
+#include "screens/home/home_gen.h"
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*UI_H*/
