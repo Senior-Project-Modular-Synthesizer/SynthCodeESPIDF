8400aa6c9ace58aa484129015ae9e7b94b64f1eb
jdaremes <samesjemerad@gmail.com>
2025-11-24 00:29:39 -0700

    All of CanQueue implemented except enableMIDI, testing needed.

--- main/CanQueue.cpp
+++ main/CanQueue.cpp
@@ -17,27 +17,66 @@
 #include "AK4619VN.hpp"
 #include "math.h"
 #include "CanQueue.hpp"
+#include "esp_log.h"
 
-CanQueue::CanQueue() {
+#define QUEUE_SIZE 128
+#define RX_GPIO_PIN 43
+#define TX_GPIO_PIN 44
+// TODO: Implement ISR handling also?
 
+static const char* TWAI = "TWAIDRIVER";
+
+// Initializer list here, because we want config responsibility to fall on the caller
+// (Largely in case of difference in operation)
+CanQueue::CanQueue(twai_handle_t twai_handle, twai_general_config_t twai_config) 
+	: twai_hdl(twai_handle), twai_cfg(twai_config) {
+	
+	xMessageQueue = xQueueCreate(QUEUE_SIZE, sizeof(twai_message_t *));
+
+	esp_err_t ret = twai_driver_install(&twai_config, nullptr, nullptr);
+
+	if (ret != ESP_OK) {
+		ESP_LOGE(TWAI, "TWAI Driver install failed: %s", esp_err_to_name(ret));
+	}
 }
 
 CanQueue::~CanQueue() {
+    if (twai_hdl) heap_caps_free(twai_hdl);
+    twai_hdl = nullptr;
 
+    if (xMessageQueue) vQueueDelete(xMessageQueue);
 }
 
 void CanQueue::start() {
-
+    twai_start();
 }
 
 void CanQueue::stop() {
-
+	twai_stop();
 }
 
-twai_message_t* getMessage() {
-	return nullptr;
+size_t CanQueue::getMessage(const twai_message_t* out) {	
+	if (xQueueReceive(xMessageQueue, &out, (TickType_t) 10) == pdPASS) {}
+	else {
+		ESP_LOGE(TWAI, "Waited for 10 ticks but did not receive message from TWAI Bus");
+		return 0;
+	}
+
+	return out->data_length_code;
 }
 
-void enableMIDI(bool enabled) {
+void CanQueue::receiveMessage() {
+	twai_message_t message;
+	
+	if (twai_receive(&message, pdMS_TO_TICKS(10000)) == ESP_OK) {
+		xQueueSendToBack( xMessageQueue, (void *) &message, (TickType_t) 0 );
+	}
+}
 
+void CanQueue::enableMIDI(bool enabled) {
+	
+}
+
+esp_err_t CanQueue::getStatusInfo(twai_status_info_t status_info) {
+	return twai_get_status_info(&status_info);
 }
--- main/CanQueue.hpp
+++ main/CanQueue.hpp
@@ -3,22 +3,30 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "driver/twai.h"
+#include "esp_log.h"
 
 class CanQueue {
 	public:
-		CanQueue();
+		CanQueue(twai_handle_t twai_hdl, twai_general_config_t twai_config);
+
 		~CanQueue();		
 
 		void start();
 
 		void stop();
 		
-		twai_message_t* getMessage();
+		size_t getMessage(const twai_message_t* out);
+
+		void receiveMessage();
 		
 		void enableMIDI(bool enabled);
 
+		esp_err_t getStatusInfo(twai_status_info_t status_info);
+
 	private:
-		
 		QueueHandle_t xMessageQueue;
 
+		twai_handle_t twai_hdl;
+
+		twai_general_config_t twai_cfg;
 };
